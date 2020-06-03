fe36e297bd654935ea44fb3ebc9486d223dc031b
Arrow <aronvinicius15@gmail.com>
2020-06-03 16:34:02 -0300

    Customizando para projeto 'dotnetcore-redis'

--- src/server-redis-custom.js
+++ src/server-redis-custom.js
@@ -7,34 +7,34 @@ const redisClient = redis.createClient(6379, '127.0.0.1');
 
 const port = 3000
 
-const getCacheRedis = (key) => {
-    return new Promise((resolve, reject) => {
-        redisClient.get(key, (err, value) => {
-            if(err){
-                reject(err)
-            } else{ 
-                resolve(value)
-            }
-        })
+const getCacheRedisAPI = (key) => {
+    return axios({
+        method: 'get',
+        url: 'https://localhost:5001/api/cache/'+key+'',
+        responseType: 'json'
     })
+    .then((res) => { return res })
+    .catch(() => { return null })
 }   
 
-const setCacheRedis = (key, value) => {
-    return new Promise((resolve, reject) => {
-        redisClient.set(key, value, (err) => {
-            if(err){
-                reject(err)
-            } else {
-                resolve(true)
-            }
-        })
+const setCacheRedisAPI = (key, value) => {
+    return axios({
+        method: 'post',
+        url: 'https://localhost:5001/api/cache',
+        data: { 
+            "Key": JSON.stringify(key),
+            "Values": JSON.stringify(value),
+            "ExpirationTimeSeconds": 30
+        }
     })
+    .then((res) => { return res })
+    .catch(() => { return null })
 }
 
 const dbViaCep = (id) => {
     return axios({
         method: 'get',
-        url: 'https://viacep.com.br/ws/'+id+'/piped/',
+        url: 'https://viacep.com.br/ws/'+id+'/json/',
         responseType: 'json'
     })
     .then((res) => { return res.data })
@@ -47,14 +47,14 @@ app.get('/', (request, response) => {
 
 app.get('/get/:id', async(request, response) => {
     let id = request.params.id
-    let key = 'get' + id
-    let value = await getCacheRedis(key)
+    let key = 'cep' + id
+    let value = await getCacheRedisAPI(key)
 
     if(value) response.send('Return from cache: ' + value)
     else {
         let idValue = await dbViaCep(id)
         if(idValue != null){
-            await setCacheRedis(key, idValue)
+            await setCacheRedisAPI(key, idValue)
             response.send('Return from ViaCEP: ' + idValue)
         }
         else response.send('CEP not exist')
